5eb470a760e4890a42c2367bd8c5dad2d049b1af
Eugeny Bobylev <ebobylev@gmail.com>
2020-01-30 14:04:15 +1000

    Инициализация репо

---
+++ HelloFlask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return f"Привет Мир!!!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
