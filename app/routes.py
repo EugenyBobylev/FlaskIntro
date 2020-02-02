8f4e155cb728a2533c579526c5f3c6323a59ae68
Eugeny Bobylev <bea@hotbox.ru>
2020-02-02 20:25:16 +1000

    fixed bug

--- app/routes.py
+++ app/routes.py
@@ -44,7 +44,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for('index'))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
