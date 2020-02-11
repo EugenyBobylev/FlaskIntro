b2f29a81c6a8170666a01a70b140b438c520b96b
Bobylev <ebobylev@gmailcom>
2020-02-11 18:02:36 +1000

    Доработка страницы профиля пользователя (отображение аватара)

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 @login.user_loader
@@ -27,6 +28,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        url = f'https://www.gravatar.com/avatr/{digest}?d=404&s={size}'
+        return url
 
 
 class Post(db.Model):
