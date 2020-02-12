2aa10c1eca681674d0779ccc3fbf1352dfacc43e
Bobylev <ebobylev@gmailcom>
2020-02-12 16:06:43 +1000

    Выполнена настройка инфрастуркутуры логирования

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     # setup connection to SQLite data base
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # configuring admin e-mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['bobylev.e.a@gmail.com']
