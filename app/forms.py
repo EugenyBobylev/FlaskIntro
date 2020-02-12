b10466087b4f5d64b4ffee475c07d4464b924ecc
Bobylev <ebobylev@gmailcom>
2020-02-12 13:53:49 +1000

    Исправлена логика обработки формы редактирования профиля

--- app/forms.py
+++ app/forms.py
@@ -30,6 +30,6 @@ class RegistrationForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired])
+    username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
