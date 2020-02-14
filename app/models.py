31856e8bb3fcf306853f7d2d8fb9bdfa1d81c372
Bobylev <ebobylev@gmailcom>
2020-02-14 11:01:27 +1000

    В моделе табл. follosers исправлены ссылки для foreign key

--- app/models.py
+++ app/models.py
@@ -12,8 +12,8 @@ def load_user(id):
 
 
 followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user_id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user_id'))
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 
