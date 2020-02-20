5d937da6b086447bc3fd3a7d1351c3750f4094df
Bobylev <ebobylev@gmailcom>
2020-02-20 13:51:42 +1000

    Добавлена инфраструктура для сброса пароля

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,8 @@ from app.models import User, Post
 from werkzeug.urls import url_parse
 from app.forms import RegistrationForm
 from app.forms import EditProfileForm
-
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 @app.before_request
 def before_request():
@@ -166,3 +167,17 @@ def explore():
     app.logger.info('GET /explore')
 
     return render_template('index.html', title="Explore", posts=posts, prev_url=prev_url, next_url=next_url)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password', form=form)
