f58d118a8cc1351ef1a05be6c1ce32b241740904
Bobylev <ebobylev@gmailcom>
2020-02-21 11:39:59 +1000

    requirements.txt - установлен пакет PySocs (для отсылки почты через proxy) EmailTest.py - заработал тест отправки письма через прокси. Отправка через mail.ru

--- EmailTest.py
+++ EmailTest.py
@@ -1,15 +1,9 @@
-import smtplib, ssl
-import email
+import smtplib
+from email.header import Header
 
-FROM = 'kitseao@yandex.ru'
-TO = ['bobylev.e.a@gmail.com']
-message = 'body of test email'
-body = "\r\n".join((
-    "From: bobylev@example.com",
-    "To: bobylev@example.com",
-    "Subject: test smtpd",
-    message
-))
+import socks
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # TODO! http://codius.ru/articles/Python_%D0%9A%D0%B0%D0%BA_%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C_%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%BE_%D0%BD%D0%B0_%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%83%D1%8E_%D0%BF%D0%BE%D1%87%D1%82%D1%83
 smtp_mail_ru = 'smtp.mail.ru'
@@ -17,18 +11,36 @@ port_mail_ru = 465
 login_mail_ru = 'masternz@mail.ru'
 pwd_mail_ru = 'Ujvbhrf1557'
 
-smtp_yandex_ru = 'smtp.yandex.ru'
-port_yandex_ru = 465
-login_yandex_ru = 'kitseao'
-pwd_yandex_ru = 'rbnctfj'
+addr_from = login_mail_ru
+addr_to = 'bobylev.e.a@gmail.com'
+
+msg = MIMEMultipart()
+msg['From'] = addr_from
+msg['To'] = addr_to
+msg['Subject'] = 'Проверка отравленного сообщения'
+body = 'Данное письмо было направлено в тестовых целях'
+msg.attach(MIMEText(body, 'plain', 'utf-8'))
+
+html = """
+<html>
+  <head></head>
+  <body>
+    <p>Фрагмент кода html</p>
+  </body>
+</html>
+"""
+msg.attach(MIMEText(html, 'html', 'utf-8'))
+
+socks.set_default_proxy(socks.HTTP, '192.168.33.212', 8080)
+socks.wrapmodule(smtplib)
 server = None
 try:
-    server = smtplib.SMTP_SSL(smtp_mail_ru, port=port_mail_ru)
+    server = smtplib.SMTP_SSL(host=smtp_mail_ru, port=port_mail_ru)
     server.login(login_mail_ru, pwd_mail_ru)
-    server.sendmail(FROM, TO, body)
+    server.sendmail(addr_from, addr_to, msg.as_string())
     print('message has sended')
 except Exception as ex:
-    print(ex)
+    print(f'Error="{ex}"')
 finally:
     if server is not None:
         server.quit()
