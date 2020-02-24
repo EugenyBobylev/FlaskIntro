e1f272ccbffaba125fec1bef8233752d6c2022d7
Eugeny Bobylev <bea@hotbox.ru>
2020-02-25 06:54:23 +1000

    В конфигурации явно прописаны настройки email

--- app/email.py
+++ app/email.py
@@ -8,6 +8,7 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
+
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
@@ -21,4 +22,4 @@ def send_password_reset_email(user):
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
-               html_body=render_template('email/reset_password_request.html', user=user, token=token))
+               html_body=render_template('email/reset_password.html', user=user, token=token))
--- config.py
+++ config.py
@@ -8,11 +8,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # configuring admin e-mail
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['bobylev.e.a@gmail.com']
+    MAIL_SERVER = 'smtp.mail.ru'
+    MAIL_USE_SSL = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = None
+    MAIL_USERNAME = 'dotnetcoder@mail.ru'
+    MAIL_PASSWORD = 'ujvbhrf1557'
+    ADMINS = ['dotnetcoder@mail.ru']
     # pagination
     POSTS_PER_PAGE = 3
