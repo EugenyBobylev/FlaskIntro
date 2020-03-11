1d6830a90bc4a653be34aff5a598f7244a22d534
Bobylev <ebobylev@gmailcom>
2020-03-12 09:48:55 +1000

    Слиты изменения из репо

--- EmailTest.py
+++ EmailTest.py
@@ -5,7 +5,7 @@ import socks
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# TODO! http://codius.ru/articles/Python_%D0%9A%D0%B0%D0%BA_%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C_%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%BE_%D0%BD%D0%B0_%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%83%D1%8E_%D0%BF%D0%BE%D1%87%D1%82%D1%83
+# TODO!
 smtp_mail_ru = 'smtp.mail.ru'
 port_mail_ru = 465
 login_mail_ru = 'masternz@mail.ru'
