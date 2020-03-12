dde03c4f97a118f58b06bc542c30622d7ff35331
Bobylev <ebobylev@gmailcom>
2020-03-12 12:14:54 +1000

    Шаблоны адаптированы под под отображение времени с использованием moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -17,9 +18,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = 'Пожалуйста, войдите, чтобы открыть эту страницу.'
 
 from app import routes, models, errors
 
